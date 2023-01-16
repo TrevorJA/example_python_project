93c6ea8b07258ab011a77d9b12d5ff531d38a6e4
TrevorJA <89143989+TrevorJA@users.noreply.github.com>
2023-01-16 16:12:58 -0500

    Edits

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Title
+# Example Python Project
+
+This repository is designed as a demonstration in organizing a Python project,
+with a focus on project structure.
--- run_program.py
+++ run_program.py
@@ -4,3 +4,17 @@ January, 2023
 
 This script is used for demonstration of Python project structure.
 """
+
+import sys
+
+from main_module import my_module
+
+
+
+def main_function(argv):
+    pass
+
+
+
+if __name__ == "__main__":
+    main_function(sys.argv)
